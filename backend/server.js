25005db4e4e2bc8ba5a3ae84e338f225e21766df
opula1234 <balajioropula@gmail.com>
2026-02-19 11:50:14 +0530

    first commit for this application

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express"
+
+const app = express()
+
+app.get("/api/notes", (req, res) => {
+    res.status(200).send("You got 50 notes")
+});
+
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({ message: "Post created successfully" });
+});
+
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "Post updated successfully" });
+});
+
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(200).json({ message: "Note deleted successfully" });
+});
+
+app.listen(5001, () => {
+    console.log("Server started on Port: 5001");
+});
