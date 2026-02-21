3ebb6b3de4fd09c273c3dae6b92e6f6607197cc4
opula1234 <balajioropula@gmail.com>
2026-02-22 00:09:36 +0530

    Feature - Added FrontEnd Code and Configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+import toast from 'react-hot-toast'
+
+const App = () => {
+  return (
+    <div data-theme="forest">
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      <button className="btn btn-active btn-link">Link</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
