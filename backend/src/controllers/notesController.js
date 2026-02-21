41f5b298ded164f3513c95a2c93301301f989072
opula1234 <balajioropula@gmail.com>
2026-02-21 09:32:42 +0530

    Feature - Added database and controller part

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,16 +1,36 @@
-export function getAllNotes(req, res) {
-    res.status(200).send("You just fetched the notes")
+import Note from "../models/Note.js"
+
+export async function getAllNotes(req, res) {
+    try {
+        const notes = await Note.find()
+        res.status(200).json(notes)
+    } catch (error) {
+        console.error(`Error in getAllNotes controller ${error}`)
+        res.status(500).json({ message: "Internal server error" })
+    }
 }
 
-export function createNote(req, res) {
-    res.status(200).json({ message: "Note created successfully!" })
+export async function createNote(req, res) {
+    try {
+        const { title, content } = req.body
+        const note = new Note({ title, content })
+        const savedNote = await note.save()
+        res.status(201).json(savedNote)
+    } catch (error) {
+        console.error(`Error in createNote controller - ${error}`)
+        res.status(500).json({ message: "Internal server error" })
+    }
 }
 
-export function updateNote(req, res) {
-    res.status(200).json({ message: "Note updated successfully!" })
+export async function updateNote(req, res) {
+    try {
+
+    } catch (error) {
+
+    }
 }
 
-export function deleteNote(req, res) {
+export async function deleteNote(req, res) {
     res.status(200).json({ message: "Note deleted successfully!" })
 }
 
