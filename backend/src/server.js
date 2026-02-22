5824d6b05931b438942f7eb8a1522d6cd30d2749
opula1234 <balajioropula@gmail.com>
2026-02-22 22:52:13 +0530

    Feature - Prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -70,17 +71,19 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve()
 
 
 // Global middleware
 // Parses incoming JSON request bodies and makes the data available on req.body.
 // Applied globally so all routes can access JSON payloads.
-app.use(cors(
-    {
-        origin: "http://localhost:5173"
-    }
-))
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors(
+        {
+            origin: "http://localhost:5173"
+        })
+    )
+}
 app.use(express.json())
 app.use(rateLimiter)
 
@@ -94,6 +97,15 @@ app.use(rateLimiter)
 // API routes
 app.use("/api/notes", notesRoutes);
 
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 // Initialize database connection
 connectDB().then(() => {
